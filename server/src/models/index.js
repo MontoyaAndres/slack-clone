2ac37e5310244c5e34fa7e53b875c009ec0f9cf6
MontoyaAndres <andresmontoyfcb@gmail.com>
2018-07-22 00:04:13 -0500

    Docker config

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,18 +1,13 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.TEST_DB || process.env.MYSQL_DATABASE,
-  'root',
-  process.env.MYSQL_ROOT_PASSWORD,
-  {
-    host: process.env.DB_HOST || 'localhost',
-    operatorsAliases: Sequelize.Op,
-    dialect: 'mysql',
-    define: {
-      underscored: true
-    }
+const sequelize = new Sequelize(process.env.TEST_DB || 'slack', 'root', 'admin', {
+  host: process.env.DB_HOST || 'localhost',
+  operatorsAliases: Sequelize.Op,
+  dialect: 'mysql',
+  define: {
+    underscored: true
   }
-);
+});
 
 const models = {
   User: sequelize.import('./user'),
