236937b4954a0f99eaf1e36defe3d321ce045666
MontoyaAndres <andresmontoyfcb@gmail.com>
2018-07-23 14:51:58 -0500

    Docker cloud and nginx added

--- server/src/resolvers/message.js
+++ server/src/resolvers/message.js
@@ -27,7 +27,7 @@ export default {
     }
   },
   Message: {
-    url: parent => (parent.url ? `http://localhost:8080/${parent.url}` : parent.url),
+    url: parent => (parent.url ? `${process.env.SERVER_URL || 'http://localhost:8080'}/${parent.url}` : parent.url),
     user: ({ user, userId }, args, { models }) => {
       if (user) {
         return user;
